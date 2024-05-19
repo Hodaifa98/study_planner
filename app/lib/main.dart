489898f352d2b961972c895de4c87dd545a475f6
hodaifa98 <houdaifa.alami@hotmail.com>
2024-05-19 18:51:45 +0100

    Created home page

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const Home());
@@ -10,14 +11,14 @@ class Home extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Home',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
-        useMaterial3: true,
-      ),
-      home: const HomePage(title: 'Home'),
-    );
+	return MaterialApp(
+	  title: 'Home',
+	  theme: ThemeData(
+		colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+		useMaterial3: true,
+	  ),
+	  home: const HomePage(title: 'Home'),
+	);
   }
 }
 
@@ -30,19 +31,20 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   /*void _incrementCounter() {
-    // Update app state.
-    setState(() {});
+	// Update app state.
+	setState(() {});
   }*/
 
 	// Rebuild app.
   	@override
   	Widget build(BuildContext context) {
-    	return Scaffold(
-        	appBar: null,
-        	body: Center(
-          		child: Column(
-            		mainAxisAlignment: MainAxisAlignment.center,
-            		children: <Widget>[
+		double screenWidth = MediaQuery.of(context).size.width;
+		return Scaffold(
+			appBar: null,
+			/*body: Center(
+		  		child: Column(
+					mainAxisAlignment: MainAxisAlignment.center,
+					children: <Widget>[
 						Text(
 							'Welcome',
 							style: Theme.of(context).textTheme.headlineMedium,
@@ -53,9 +55,37 @@ class _HomePageState extends State<HomePage> {
 						Text(
 							'StudyPlanner',
 							style: Theme.of(context).textTheme.bodyLarge,
-					),
-            	],
-          ),
-        ));
+						),
+					],
+		  		),
+			)*/
+			body: DecoratedBox(
+				decoration: const BoxDecoration(
+					image: DecorationImage(
+						image: AssetImage('assets/images/background.jpg'),
+						fit: BoxFit.cover
+					)
+				),
+				child: Center(
+					child: Column(
+						mainAxisAlignment: MainAxisAlignment.center,
+						children: <Widget>[
+							Image(
+								image: const AssetImage('assets/images/logo.png'),
+								width: screenWidth * 0.18
+							),
+							const Text(
+								'StudyPlanner',
+								style: TextStyle (
+									fontFamily: 'Roboto',
+									fontWeight: FontWeight.bold,
+									fontSize: 35.0
+								)
+							),
+						],
+		  			),
+				),
+			)
+		);
   	}
 }
